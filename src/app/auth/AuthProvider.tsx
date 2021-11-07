e92ab716e9137cd409b2ea17726b851a3e82e619
Marko Arsic <marsicdev@gmail.com>
2021-11-07 17:10:13 +0100

    Fix useReducer import

--- src/app/auth/AuthProvider.tsx
+++ src/app/auth/AuthProvider.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 import * as authActions from './authActions'
 import { AuthDispatchCtx, AuthStateCtx } from './auth-context'
-import { useReducer } from 'react-router/node_modules/@types/react'
 
 export const AuthProvider: React.FC = ({ children }) => {
     const { defaultAuthState, authReducer } = authActions
